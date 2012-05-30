39c6c01321304620bc7f501030b5b8f4e4c13130
Chris Chilvers <chilversc@gmail.com>
2012-05-30 17:37:19 +0100

    use same text as Gallery3 so translations work

--- views/error_404.html.php
+++ views/error_404.html.php
@@ -1,11 +1,11 @@
 <?php defined("SYSPATH") or die("No direct script access.") ?>
 <div id="g-error">
   <h1>
-    <?= t("Page not found!") ?>
+    <?= t("Dang...  Page not found!") ?>
   </h1>
   <? if ($is_guest): ?>
     <h2>
-      <?= t("You're currently not signed in.") ?>
+      <?= t("Hey wait, you're not signed in yet!") ?>
     </h2>
     <p>
        <?= t("Maybe the page exists, but is only visible to authorized users.") ?>
